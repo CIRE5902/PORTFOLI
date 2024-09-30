b743856023f3baec66e1eff1f11e2fa30a8e9584
EricPerez <ericperezvera02@gmail.com>
2024-09-30 21:46:37 +0200

    Primera versión del portfolio

--- js/index.js
+++ js/index.js
@@ -1,19 +1,30 @@
-// Obtener los elementos del DOM
-const menuToggle = document.getElementById('menu-toggle');
-const sidebar = document.querySelector('.sidebar');
+// Seleccionamos el body, el icono y el botón de cambio de tema
+const body = document.body;
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
 
-// Función para alternar el estado del menú
-menuToggle.addEventListener('click', () => {
-    // Alternar la clase 'open' en el sidebar
-    sidebar.classList.toggle('open');
-    
-    // Mover el icono del menú junto con el sidebar
-    menuToggle.classList.toggle('menu-open');
-    
-    // Cambiar el icono entre '☰' y '×'
-    if (sidebar.classList.contains('open')) {
-        menuToggle.textContent = '×';  // Mostrar la cruz cuando el menú esté abierto
+// Manejador del evento para cambiar entre modo claro y oscuro
+themeToggle.addEventListener('click', function () {
+    body.classList.toggle('light-mode');
+    body.classList.toggle('dark-mode');
+
+    // Cambiar el icono y el tooltip según el modo actual
+    if (body.classList.contains('light-mode')) {
+        themeIcon.src = 'images/sun.png'; // Imagen para el modo claro
+        themeIcon.alt = 'Modo Claro';
+        themeToggle.setAttribute('data-tooltip', 'Modo Oscuro');
     } else {
-        menuToggle.textContent = '☰';  // Mostrar el icono de menú cuando esté cerrado
+        themeIcon.src = 'images/moon.png'; // Imagen para el modo oscuro
+        themeIcon.alt = 'Modo Oscuro';
+        themeToggle.setAttribute('data-tooltip', 'Modo Claro');
     }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.getElementById('menu-btn');
+    const sidebar = document.getElementById('sidebar');
+
+    menuBtn.addEventListener('click', function() {
+        sidebar.classList.toggle('show'); // Alternar la clase 'show' para abrir/cerrar el menú
+    });
 });
