cb24fc8c9ad1b17dd1fba641327cd1ed0c97e350
EricPerez <ericperezvera02@gmail.com>
2024-10-05 17:56:27 +0200

    ahora se cambie el icono segun el tema

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-// Seleccionamos el body, el icono y el botón de cambio de tema
+// Seleccionamos el body y el botón de cambio de tema
 const body = document.body;
 const themeToggle = document.getElementById('theme-toggle');
-const themeIcon = document.getElementById('theme-icon');
+const themeIcon = themeToggle.querySelector('i'); // Seleccionamos el icono dentro del botón
 
 // Manejador del evento para cambiar entre modo claro y oscuro
 themeToggle.addEventListener('click', function () {
@@ -10,12 +10,10 @@ themeToggle.addEventListener('click', function () {
 
     // Cambiar el icono y el tooltip según el modo actual
     if (body.classList.contains('light-mode')) {
-        themeIcon.src = 'images/sun.png'; // Imagen para el modo claro
-        themeIcon.alt = 'Modo Claro';
+        themeIcon.classList.replace('fa-moon', 'fa-sun'); // Cambiamos el icono de luna a sol
         themeToggle.setAttribute('data-tooltip', 'Modo Oscuro');
     } else {
-        themeIcon.src = 'images/moon.png'; // Imagen para el modo oscuro
-        themeIcon.alt = 'Modo Oscuro';
+        themeIcon.classList.replace('fa-sun', 'fa-moon'); // Cambiamos el icono de sol a luna
         themeToggle.setAttribute('data-tooltip', 'Modo Claro');
     }
 });
