c5d22734dff47e529907c6d34a39118956963aec
EricPerez <95928362+CIRE5902@users.noreply.github.com>
2025-04-07 12:58:56 +0200

    avance

--- js/emailForm.js
+++ js/emailForm.js
@@ -1,21 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-    emailjs.init("SCiElbajNfEMtFbNb");
-  
-    const form = document.querySelector("form");
-  
-    if (form) {
-      form.addEventListener("submit", function (e) {
-        e.preventDefault();
-  
-        emailjs.sendForm("service_9sk31cp", "template_c7r2mmg", this).then(
-            function () {
-            alert("Correo enviado!");
-          },
-          function (error) {
-            console.error("Error al enviar", error);
-          }
-        );
-      });
-    }
-  });
-  
+// const form = document.getElementById('contact-form');
+// const responseMessage = document.getElementById('response-message');
+
+// form.addEventListener('submit', async function (e) {
+//   // e.preventDefault(); 
+
+//   const formData = new FormData(form);
+
+//   try {
+//     const response = await fetch('https://formspree.io/f/xwplwbvo', {
+//       method: 'POST',
+//       body: formData,
+//       headers: {
+//         'Accept': 'application/json'
+//       }
+//     });
+
+//     if (response.ok) {
+//       responseMessage.textContent = '¡Mensaje enviado con éxito!';
+//       responseMessage.style.color = 'green';
+//       form.reset(); 
+//     } else {
+//       responseMessage.textContent = 'Ocurrió un error al enviar tu mensaje.';
+//       responseMessage.style.color = 'red';
+//     }
+//   } catch (error) {
+//     responseMessage.textContent = 'No se pudo conectar con el servidor.';
+//     responseMessage.style.color = 'red';
+//   }
+// });
