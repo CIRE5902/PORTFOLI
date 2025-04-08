ace8ee5bd8c3cc25ca6e761de409d524fb15bb67
EricPerez <95928362+CIRE5902@users.noreply.github.com>
2025-04-08 09:49:37 +0200

    prueba redireccionamiento netlify

--- js/emailForm.js
+++ js/emailForm.js
@@ -1,30 +1,28 @@
-// const form = document.getElementById('contact-form');
-// const responseMessage = document.getElementById('response-message');
-
-// form.addEventListener('submit', async function (e) {
-//   // e.preventDefault(); 
-
-//   const formData = new FormData(form);
-
-//   try {
-//     const response = await fetch('https://formspree.io/f/xwplwbvo', {
-//       method: 'POST',
-//       body: formData,
-//       headers: {
-//         'Accept': 'application/json'
-//       }
-//     });
-
-//     if (response.ok) {
-//       responseMessage.textContent = '¡Mensaje enviado con éxito!';
-//       responseMessage.style.color = 'green';
-//       form.reset(); 
-//     } else {
-//       responseMessage.textContent = 'Ocurrió un error al enviar tu mensaje.';
-//       responseMessage.style.color = 'red';
-//     }
-//   } catch (error) {
-//     responseMessage.textContent = 'No se pudo conectar con el servidor.';
-//     responseMessage.style.color = 'red';
-//   }
-// });
+// Función que maneja el envío del formulario sin redirección
+function handleFormSubmission(e) {
+    e.preventDefault(); // Previene la redirección de Netlify
+  
+    const form = e.target;
+    const formData = new FormData(form);
+  
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData).toString(),
+    })
+    .then((response) => {
+      if (response.ok) {
+        document.getElementById("form-response").style.display = "block";
+        document.getElementById("form-response").innerText = "¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.";
+        form.reset(); // Limpiar el formulario
+      } else {
+        document.getElementById("form-response").style.display = "block";
+        document.getElementById("form-response").innerText = "Hubo un error al enviar el mensaje. Por favor, inténtalo nuevamente.";
+      }
+    })
+    .catch(() => {
+      document.getElementById("form-response").style.display = "block";
+      document.getElementById("form-response").innerText = "Hubo un error al enviar el mensaje. Por favor, inténtalo nuevamente.";
+    });
+  }
+  
