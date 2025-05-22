7850933a5fefa806cbbeaba86f8338be373eb23c
EricPerez <95928362+CIRE5902@users.noreply.github.com>
2025-05-22 12:00:04 +0200

    pagina about acabada

--- js/language.js
+++ js/language.js
@@ -24,6 +24,19 @@ const translations = {
       studiesTitle: "Estudios",
       projectsTitle: "Proyectos",
       stackTitle: "Stack"
+    }, about: {
+      aboutTitle: "SOBRE MÍ",
+      firstInfoAbout: "Me llamo <strong>Eric</strong> —aunque mis amigos me llaman <strong>“Cirelema”</strong>—, tengo <strong>22 años</strong> y soy de <strong>Barcelona, España</strong>. Al principio consideré dedicarme al <strong>cuidado de animales</strong>, ya que siempre ha sido una de mis grandes pasiones. Sin embargo, tras unas prácticas, descubrí que también tiene momentos muy duros, así que decidí dar el salto a la <strong>programación</strong>.",
+      secondInfoAbout: "Desde entonces, disfruto creando experiencias web <strong>intuitivas</strong> y <strong>visualmente atractivas</strong>. Me apasiona el <strong>lado visual</strong> y el <strong>diseño de interfaces</strong>, porque creo que la estética es clave para ofrecer la mejor experiencia al usuario.",
+      thirdInfoAbout: "Terminé mis estudios como <strong>Desarrollador Web</strong> y <strong>Administrador de Sistemas</strong>. Durante el ciclo formativo, destaqué por ayudar a mis compañeros a aprender <strong>CSS</strong> — compartir conocimiento y fomentar el buen diseño son valores esenciales para mí.",
+      quarterInfoAbout: "Actualmente tengo experiencia desarrollando <strong>aplicaciones web</strong>, y mi objetivo es unir mis dos grandes pasiones —la <strong>programación</strong> y el <strong>mundo animal</strong>— creando proyectos que ayuden a mejorar la vida de los animales.",
+      fifthInfoAbout: "Fuera del ámbito tecnológico, me apasiona el <strong>mundo del motor</strong>, el <strong>deporte</strong>, la <strong>moda</strong> a mi manera y los <strong>animales</strong>. También valoro mucho la <strong>soledad</strong>, aunque disfruto pasar tiempo con mi <strong>familia</strong> y <strong>amigos cercanos</strong>. Me encantan las películas sentimentales y de superación, especialmente las que dejan un mensaje, como <em>El gigante de hierro</em>, <em>El señor de los anillos</em> o <em>Soy leyenda</em>.",
+      firstFollow: "Sígueme en Spotify",
+      secondFollow: "Sígueme en Instagram",
+      thirdFollow: "Sígueme en LinkedIn",
+      quarterFollow: "Enviar correo",
+      fifthFollow: "Sígueme en Github",
+      sixthFollow: "Sígueme en Figma"
     },
     contact: {
       contactTitle: "CONTACTAME",
@@ -59,6 +72,19 @@ const translations = {
       studiesTitle: "Studies",
       projectsTitle: "Projects",
       stackTitle: "Stack"
+    }, about: {
+      aboutTitle: "ABOUT ME",
+      firstInfoAbout: "My name is <strong>Eric</strong> —although my friends call me <strong>“Cirelema”</strong>—, I'm <strong>22 years old</strong> and from <strong>Barcelona, Spain</strong>. At first, I considered pursuing a career in the <strong>animal care sector</strong>, as it's always been one of my greatest passions. However, after an internship, I discovered that it also has very tough moments, so I decided to make the leap into <strong>programming</strong>.",
+      secondInfoAbout: "Since then, I've enjoyed creating <strong>intuitive</strong> and <strong>visually appealing</strong> web experiences. I'm passionate about the <strong>visual side</strong> and <strong>interface design</strong>, because I believe aesthetics are key to delivering the best possible user experience.",
+      thirdInfoAbout: "I completed my studies as a <strong>Web Developer</strong> and <strong>Systems Administrator</strong>. During my Higher Vocational Training, I stood out by helping my classmates learn <strong>CSS</strong> — sharing knowledge and promoting good design are essential values for me.",
+      quarterInfoAbout: "I now have experience developing <strong>web applications</strong>, and my goal is to combine my two greatest passions —<strong>programming</strong> and the <strong>animal world</strong>— by creating projects that help improve the lives of animals.",
+      fifthInfoAbout: "Outside of tech, I’m passionate about the <strong>world of motor vehicles</strong>, <strong>sports</strong>, <strong>fashion</strong> in my own style, and <strong>animals</strong>. I also value <strong>solitude</strong>, but I truly enjoy spending time with my <strong>family</strong> and <strong>close friends</strong>. Emotional and inspiring movies, especially those with a meaningful message, are my favorites — like <em>The Iron Giant</em>, <em>The Lord of the Rings</em>, or <em>I Am Legend</em>.",
+      firstFollow: "Follow on Spotify",
+      secondFollow: "Follow on Instagram",
+      thirdFollow: "Follow on LinkedIn",
+      quarterFollow: "Send Email",
+      fifthFollow: "Follow on Github",
+      sixthFollow: "Follow on Figma"
     },
     contact: {
       contactTitle: "CONTACT ME",
@@ -94,6 +120,19 @@ const translations = {
       studiesTitle: "Estudis",
       projectsTitle: "Projectes",
       stackTitle: "Stack"
+    }, about: {
+      aboutTitle: "SOBRE MI",
+      firstInfoAbout: "Em dic <strong>Eric</strong> —encara que els meus amics em diuen <strong>“Cirelema”</strong>—, tinc <strong>22 anys</strong> i sóc de <strong>Barcelona, Espanya</strong>. Al principi volia dedicar-me al <strong>sector del benestar animal</strong>, ja que sempre ha estat una de les meves grans passions. No obstant això, després d’unes pràctiques, vaig veure que també té moments molt durs, així que vaig decidir fer el salt a la <strong>programació</strong>.",
+      secondInfoAbout: "Des de llavors, m’encanta crear experiències web <strong>intuïtives</strong> i <strong>visualment atractives</strong>. Em fascina la <strong>part visual</strong> i el <strong>disseny d’interfícies</strong>, perquè crec que l’estètica és clau per oferir la millor experiència d’usuari.",
+      thirdInfoAbout: "Vaig acabar els meus estudis com a <strong>Desenvolupador Web</strong> i <strong>Administrador de Sistemes</strong>. Durant el cicle formatiu, vaig destacar ajudant els meus companys a aprendre <strong>CSS</strong> — compartir coneixement i fomentar el bon disseny són valors essencials per a mi.",
+      quarterInfoAbout: "Actualment tinc experiència desenvolupant <strong>aplicacions web</strong>, i el meu objectiu és combinar les meves dues grans passions —la <strong>programació</strong> i el <strong>món animal</strong>— creant projectes que ajudin a millorar la vida dels animals.",
+      fifthInfoAbout: "Fora de la tecnologia, m’apassiona el <strong>món del motor</strong>, els <strong>esports</strong>, la <strong>moda</strong> al meu estil i els <strong>animals</strong>. També valoro molt la <strong>solitud</strong>, encara que gaudeixo passant temps amb la meva <strong>família</strong> i els <strong>amics propers</strong>. M'encanten les pel·lícules emocionals i de superació, especialment les que tenen un missatge, com <em>El gegant de ferro</em>, <em>El senyor dels anells</em> o <em>Sóc llegenda</em>.",
+      firstFollow: "Segueix-me a Spotify",
+      secondFollow: "Segueix-me a Instagram",
+      thirdFollow: "Segueix-me a LinkedIn",
+      quarterFollow: "Envia un correu",
+      fifthFollow: "Segueix-me a Github",
+      sixthFollow: "Segueix-me a Figma"
     },
     contact: {
       contactTitle: "CONTACTA'M",
@@ -115,7 +154,11 @@ function changeLanguage(language, page) {
     const el = document.getElementById(id);
     if (el) {
       const text = content[id];
-      el[text.includes('<span') ? 'innerHTML' : 'textContent'] = text;
+      if (/<[^>]+>/.test(text)) {
+        el.innerHTML = text;
+      } else {
+        el.textContent = text;
+      }
     }
   }
 
